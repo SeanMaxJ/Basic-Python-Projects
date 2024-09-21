2c5fb220bd9ad0e66ef1593412eb4f3c897c625e
unknown <seanmaxofficial@gmail.com>
2024-09-21 22:39:28 +0530

    Simplified Code

--- NumberGuessingGame/NumberGuessingGame.py
+++ NumberGuessingGame/NumberGuessingGame.py
@@ -8,7 +8,7 @@ def game() :
     count = 0
     while guess != x :
         count += 1
-        guess = int(input("\nEnter Your Guess (1 - 100) : "))
+        guess = int(input("\nEnter Your Guess ! (1 - 100) : "))
         if (guess > x) :
             print("Guess Lower !")
             
@@ -19,11 +19,10 @@ def game() :
             print("\nAffirmative, Good Job ;)\nAttempts : ", count)
             a = 1
             return a 
-            
-while True :
-    print("\tSMJ's NUMBER GUESSING GAME !!!")
-    print("\t-----------------------")
-    guess = ("\nEnter Your Guess ! (1-100) : ")
+print("\tSMJ's NUMBER GUESSING GAME !!!")
+print("\t------------------------------")
+
+while True : 
     game()
     print("\nThank You For Playing !")
     r = input(("\nWould You Like To Play Again ? (Yes/No) : "))
